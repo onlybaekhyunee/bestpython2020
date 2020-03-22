8d4491836c70a02c97c12fb4fa8dd1363765b9c2
onlybaekhyunee <olbaekhyunee@163.com>
2020-03-23 00:36:06 +0800

    5test

--- helloworld.py
+++ helloworld.py
@@ -1,2 +1,18 @@
-temp=input('我可以帮你转换二进制，请输入一个数字吧')
-print(bin(int(temp)))
+a='我是str1'
+b=a
+print('游戏1：\na='+a,'clone_a='+b)
+temp = input('请输入任意内容，改变a的值')
+a=temp
+print('a='+a,'clone_a='+b)
+c=['我','是','str2']
+d=c
+print('游戏2：\nc=')
+print(c)
+print('clone_c=')
+print(d)
+temp = input('请输入任意内容，改变c的值')
+c[-1]=temp
+print('c=')
+print(c)
+print('clone_c=')
+print(d)
