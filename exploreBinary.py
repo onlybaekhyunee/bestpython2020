4c54dfe6d4b60b96db2e2263cf33fe6379fcefae
onlybaekhyunee <olbaekhyunee@163.com>
2020-03-23 22:17:53 +0800

    二进制练手

---
+++ exploreBinary.py
@@ -0,0 +1,19 @@
+n=int(input('请输入一个数字：'))
+print('该数字的二进制为：'+str(bin(n)))
+temp =n
+m = 1
+result1 = 0
+result0 = 0
+i = 0
+
+while i < 32:
+    i += 1
+    if m&n :
+        result1 += 1              
+    m = m << 1
+    temp = temp>>1
+    if (not temp) and (not result0):
+        result0 = i
+print('该数字二进制编码中含有1的个数为：'+str(result1))
+print('该数字二进制编码中含有0的个数为：'+str(result0-result1))
+
