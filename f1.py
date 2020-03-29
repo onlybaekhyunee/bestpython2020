9a89f1b8c0a418e70df86dacac61709492cc37bd
onlybaekhyunee <olbaekhyunee@163.com>
2020-03-29 22:09:33 +0800

    add multiparam fun

--- f1.py
+++ f1.py
@@ -10,15 +10,44 @@ testpackage.sys.setrecursionlimit(1000)
 
 attack = False
 
-def multiparafun(skill1,skill2,skill3):
-    print(attack)
+def skillinfofun(**skinfo):
+    # 可变关键字参数
+    print("Skinfo's type is "+str(type(skinfo)))
+    for key,value in skinfo.items():
+        print(key+': '+value)
+    
+def multiparafun(skill1=0,skill2=0,skill3=0):
+    # 关键字参数，增加可读性
+    print('Attack is '+str(attack))
+    demage1=demage2=demage3=0
+    #链式赋值，一次性给三个变量赋相同的值
     if attack:
         demage1 = skill1*5
         demage2 = skill1*2+skill2
-        demage3 = skill3*88/3
-    else:
-        demage1=demage2=demage3=0
-        # 一次性给三个变量赋相同的值
+        demage3 = skill3*88/3  
+    print("Skill1's demage is "+str(demage1))
+    print("Skill2's demage is "+str(demage2))
+    print("Skill3's demage is "+str(demage3))
+    totald = round(demage1+demage2+demage3,2)
+    print('This is '+str(totald)+' demage in total')
     return demage1,demage2,demage3
-
     
+def variparafun(isattack,*demage,istotal=True):
+    # 必须形参，可变参数，关键字参数
+    # 可变参数要在必须参数的后面，一个*为可变普通参数，两个*为关键字可变参数
+    # 有多种参数时，关键字参数调用时必须明确指出关键字
+    i = totald = 0
+    print('Attack is '+str(attack))
+    print("Demage's type is "+str(type(demage)))
+    # tuple
+    if isattack:
+        for s in demage: 
+            i += 1
+            totald += s
+            print("Skill"+str(i)+"'s demage is "+str(s))
+
+    totald = round(totald,2)
+    if istotal:
+        print('This is '+str(totald)+' demage in total')
+    return
+
--- f2.py
+++ f2.py
@@ -1,12 +1,18 @@
 import f1
 
 f1.attack = True
-r = f1.multiparafun(3,4,5)
-d1,d2,d3 = r
+
+f1.skillinfofun(SkillV='Ice Magic,AOE',SkillE='Single Target Damage，can not move for 3 minutes',skillF='High Ice Magic AOE,can be breaked')
+# 可变关键字参数传参方法
+print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+
+f1.attack = True
+r1 = f1.multiparafun(skill1=6,skill2=7,skill3=8)
+d1,d2,d3 = r1
 # 序列解包
-print("Skill1's demage is "+str(d1))
-print("Skill2's demage is "+str(d2))
-print("Skill3's demage is "+str(d3))
-alld = d1+d2+d3
-alld = round(alld,2)
-print('This is '+str(alld)+' demage in total')
+print("returnpama's type is "+str(type(r1)))
+# tuple
+print(d1,d2,d3)
+print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+
+f1.variparafun(True,20,300.698,1256,31,istotal = True)
