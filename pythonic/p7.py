c7abf5cfbd9ab2794ecf5e944fcfdda4f48e697a
onlybaekhyunee <olbaekhyunee@163.com>
2020-04-21 18:08:27 +0800

    key word f learning

--- pythonic/p7.py
+++ pythonic/p7.py
@@ -9,14 +9,15 @@ import re
 # 例1：一般应用
 a = 'I like Python'
 if (b:=len(a))>5:
-    print(a+'.The length of this sentence is '+str(b))
+    # f关键字来拼接字符串
+    print(f'{a}.The length of this sentence is {b}')
 
 # 例2：正则表达式应用
 c = 'The mall is having a sale. The discount for books is 70%,the discount for shoes is 50% '
 if (d:=re.findall('(\d+)%',c)):
     discount = map(lambda x: float(x)/100.0,d)
     print(list(discount))
-    
+
 # 例3:列表推导式应用
 def f(key,value):
     if int(value) >= 18:
